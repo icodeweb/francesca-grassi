a15fdb2321829fcb85b9c46cb19325121716779d
Ibrahim Ismail <ebrahemesmael@gmail.com>
2025-02-17 14:06:38 +0200

    add abilty to display imgs with diffrent formats

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-function loadPage() {
-    document.getElementById("root").innerHTML = `
-    <div class="textbox">
-        <a class="contact" href="mailto:fg@francescagrassi.info" rel="mailto">Contact</a>
-        <p>Francesca Grassi is a New York-based designer and educator working in visual design,
-            storytelling, strategy, and editorial design.</p>
-    </div>
-    <div class="container">
-        <div class="column" id="one">
-        </div>
-        <div class="column" id="two">
-        </div>
-    </div>
-`
-}
-loadPage()
+let imagesCount = 32;
 
+// set this to the number of images.
+for (let index = 1; index <= imagesCount; index++) {
+    let image = document.createElement("img");
+    image.src = `/img/img${index}.jpg`; // default format => jpg 
 
-function generateContent(elemntId) {
-    let containerElemnt = document.getElementById(`${elemntId}`)
-    let sideHTML = ``
-    if (elemntId === "one") {
-        for (let index = 1; index < 17; index++) {
-            if (index === 5 || index === 10) {
-                sideHTML += `<img src="./img/img${index}.gif" />`
-            } else {
-                sideHTML += `<img src="./img/img${index}.jpg" />`
-            }
-        }
+    image.onerror = function () {
+        image.src = `/img/img${index}.png`; //if not jpg try png
+
+        image.onerror = function () {
+            image.src = `/img/img${index}.gif`; //if not jpg or png try gif
+        };
+    };
+
+    if (index <= imagesCount / columnsCount) {
+        document.getElementById("one").appendChild(image);
     } else {
-        for (let index = 17; index < 33; index++) {
-            if (index === 17 || index === 24) {
-                sideHTML += `<img src="./img/img${index}.gif" />`
-            } else {
-                sideHTML += `<img src="./img/img${index}.jpg" />`
-            }
-        }
+        document.getElementById("two").appendChild(image);
     }
-    containerElemnt.innerHTML = sideHTML;
 }
 
-generateContent("one")
-generateContent("two")
